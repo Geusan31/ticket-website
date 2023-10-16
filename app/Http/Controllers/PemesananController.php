3da0d8437146626f76a5bb7a5359e23d259edfba
Geusan31 <edurais.ariadaffa@gmail.com>
2023-10-16 14:51:08 +0700

    change pemesanan

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\pemesanan;
+use App\Models\Penumpang;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
 
 class PemesananController extends Controller
 {
@@ -11,6 +15,20 @@ class PemesananController extends Controller
     {
         $kodeTerbesar = Pemesanan::max('kode_pemesanan');
         $kodeBaru = str_pad($kodeTerbesar + 1, 3, '0', STR_PAD_LEFT);
+        
+        $result = $request->validate([
+            'kode_kursi' => "required",
+            'id_rute' => "required",
+            'tujuan' => 'required',
+            'tanggal_berangkat' => 'required',
+            'jam_cekin' => 'required',
+            'jam_berangkat' => 'required',
+            'total_bayar' => 'required',
+        ]);
+
+        $result['kode_pemesanan'] = $kodeBaru;
+        $result['tanggal_pemesanan'] = Carbon::now()->format('Y-m-d');
+        $result['id_pelanggan'] = Auth::guard('penumpang')->user()->id_penumpang;
     }
 
     public function update(Request $request, $id)
